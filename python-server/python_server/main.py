bfb66429302aef73a6a38dd666840ffddf8de2ed
23prime <23.prime.37@gmail.com>
2022-05-26 15:20:08 +0900

    Add gRPC Reflection

--- python-server/python_server/main.py
+++ python-server/python_server/main.py
@@ -5,6 +5,8 @@ from asyncio.log import logger
 from concurrent import futures
 
 import grpc
+from grpc_reflection.v1alpha import reflection
+
 import helloworld_pb2
 import helloworld_pb2_grpc
 
@@ -20,7 +22,14 @@ class Greeter(helloworld_pb2_grpc.GreeterServicer):
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+
     helloworld_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
+    SERVICE_NAMES = (
+        helloworld_pb2.DESCRIPTOR.services_by_name["Greeter"].full_name,
+        reflection.SERVICE_NAME,
+    )
+    reflection.enable_server_reflection(SERVICE_NAMES, server)
+
     server.add_insecure_port("[::]:50051")
     server.start()
     server.wait_for_termination()
